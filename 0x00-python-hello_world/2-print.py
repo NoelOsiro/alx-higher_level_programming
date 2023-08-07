145eb56632e2182212f6214b6ca32bcc2d30efa2
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2023-08-07 13:10:41 +0000

    print

--- 0x00-python-hello_world/2-print.py
+++ 0x00-python-hello_world/2-print.py
@@ -1,6 +1,2 @@
 #!/usr/bin/python3
-"""
-This is a simple Python script that prints:
-'Programming is like building a multilingual puzzle'
-"""
 print("\"Programming is like building a multilingual puzzle")
