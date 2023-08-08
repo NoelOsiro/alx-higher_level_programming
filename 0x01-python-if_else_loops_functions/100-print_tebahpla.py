c9b2c011d6a7923e7dc409d986a07c440c6300c1
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2023-08-08 12:45:40 +0000

    whitespace

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 
 for i in range(122, 96, -1):
-    print(chr(i).lower() if i % 2 == 0 else chr(i).upper(), end="")
+    char = chr(i).lower() if i % 2 == 0 else chr(i).upper()
+    print("{}".format(char), end="")
--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
 """Print the alphabet in lowercase, not followed by a new line."""
+alphabet = ""
 for i in range(97, 123):
-    print(chr(i), end="")
+    alphabet += chr(i)
+
+print("{}".format(alphabet), end="")
