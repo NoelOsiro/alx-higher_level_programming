9eeb213928292069d0b105292c0fe322392f7741
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2023-08-16 20:02:14 +0000

    permissions

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-# 1-search_replace.py
-# Brennan D Baraban <375@holbertonschool.com
-
 
 def search_replace(my_list, search, replace):
     """Replace all occurrences of an element by another in a new list."""
@@ -10,3 +7,4 @@ def search_replace(my_list, search, replace):
         if new_list[i] == search:
             new_list[i] = replace
     return (new_list)
+
