b88c61b4639c0a9af3f3ee35c9d393d5979d6e95
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2023-08-28 23:44:12 +0000

    python exceptions

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python3
+
+
 def safe_print_integer(value):
+    """Print an integer with "{:d}".format().
+
+    Args:
+        value (int): The integer to print.
+
+    Returns:
+        If a TypeError or ValueError occurs - False.
+        Otherwise - True.
+    """
     try:
         formatted_value = "{:d}".format(value)
         print(formatted_value)
         return True
-    except:
-        return False
+    except (TypeError, ValueError):
+        return (False)
