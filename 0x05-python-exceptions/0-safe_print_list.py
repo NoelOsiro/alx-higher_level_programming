8cd2595b3d41f668df910e82da9fe70c98608551
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2023-08-28 23:30:03 +0000

    python exceptions

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        printed_count = 0
+        for element in my_list:
+            if printed_count < x:
+                print(element,end='')
+                printed_count += 1
+            else:
+                break
+        print()
+        return printed_count
+    except:
+        return printed_count
---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    try:
+        printed_count = 0
+        for element in my_list:
+            if printed_count < x:
+                try:
+                    formatted_element = "{:d}".format(element)
+                    print(formatted_element, end="")
+                    printed_count += 1
+                except (ValueError, TypeError):
+                    pass  # Skip non-integer elements
+            else:
+                break
+        print()
+        return printed_count
+    except:
+        return printed_count
