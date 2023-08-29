50dd2b3b8d22b516db1fc6970ee5d412adf1c188
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2023-08-29 00:12:06 +0000

    python exceptions

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -15,7 +15,7 @@ def safe_function(fct, *args):
     """
     try:
         result = fct(*args)
-        return (result)
-    except (ValueError, TypeError):
-        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
-        return (None)
+        return result
+    except Exception as e:
+        print("Exception:", e, file=sys.stderr)
+        return None
