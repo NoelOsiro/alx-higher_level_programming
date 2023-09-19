8ad09369059cbcb371e060ebff0c912614adfa35
Noel Osiro <noel.osiro@azubiafrica.org>
2023-09-20 02:21:23 +0300

    Circle

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -42,6 +42,68 @@ class TestBase_instantiation(unittest.TestCase):
         # Check if the instance has the ID passed as an argument.
         self.assertEqual(b.id, 89)
 
+    def test_base_constructor_with_id(self):
+        b1 = Base(5)
+        self.assertEqual(b1.id, 5)
+
+    def test_base_constructor_without_id(self):
+        b1 = Base()
+        b2 = Base()
+        self.assertEqual(b1.id, 1)
+        self.assertEqual(b2.id, 2)
+
+    def test_to_json_string(self):
+        r1 = Rectangle(10, 7, 2, 8)
+        r2 = Rectangle(2, 4)
+        json_str = Rectangle.to_json_string(
+            [r1.to_dictionary(),
+             r2.to_dictionary()])
+        self.assertTrue(isinstance(json_str, str))
+        self.assertEqual(json_str,
+                         '[{"id": 1, "width": 10, "height": 7, "x": 2, "y": 8}, {"id": 2, "width": 2, "height": 4, "x": 0, "y": 0}]')
+
+    def test_from_json_string(self):
+        json_str = '[{"id": 1, "width": 10, "height": 7, "x": 2, "y": 8}, {"id": 2, "width": 2, "height": 4, "x": 0, "y": 0}]'
+        json_list = Rectangle.from_json_string(json_str)
+        self.assertTrue(isinstance(json_list, list))
+        self.assertEqual(len(json_list), 2)
+        self.assertTrue(isinstance(json_list[0], dict))
+        self.assertEqual(json_list[0]['width'], 10)
+
+    def test_save_to_file_and_load_from_file(self):
+        r1 = Rectangle(10, 7, 2, 8)
+        r2 = Rectangle(2, 4)
+        Rectangle.save_to_file([r1, r2])
+        loaded_rectangles = Rectangle.load_from_file()
+        self.assertEqual(len(loaded_rectangles), 2)
+        self.assertEqual(loaded_rectangles[0].width, 10)
+        self.assertEqual(loaded_rectangles[1].height, 4)
+
+    def test_create_method(self):
+        r_dict = {
+            'id': 5,
+            'width': 3,
+            'height': 4,
+            'x': 1,
+            'y': 2}
+        r = Rectangle.create(**r_dict)
+        self.assertEqual(r.id, 5)
+        self.assertEqual(r.width, 3)
+        self.assertEqual(r.x, 1)
+
+    def test_save_to_file_csv_and_load_from_file_csv(self):
+        s1 = Square(5)
+        s2 = Square(7, 9, 1)
+        Square.save_to_file_csv([s1, s2])
+        loaded_squares = Square.load_from_file_csv()
+        self.assertEqual(len(loaded_squares), 2)
+        self.assertEqual(loaded_squares[0].size, 5)
+        self.assertEqual(loaded_squares[1].y, 1)
+
+    def test_draw_method(self):
+        r1 = Rectangle(10, 7, 2, 8)
+        s1 = Square(5)
+        self.assertIsNone(Base.draw([r1], [s1]))
 
 if __name__ == "__main__":
     unittest.main()
