b5462860e32997762b46a3497fe82ef9112a15b0
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2023-11-13 22:02:12 +0000

    3-value

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,2 @@
 #!/usr/bin/node
-const numArgs = process.argv.length;
-if (numArgs < 3) {
-  console.log('No Arguement');
-} else {
-  console.log(process.argv[2]);
-}
+console.log(typeof process.argv[2] === 'undefined' ? 'No argument' : process.argv[2]);
