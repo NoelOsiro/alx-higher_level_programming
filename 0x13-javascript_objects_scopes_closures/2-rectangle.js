0fdc8f859f2d3e70edde9e10642549ccce20edc7
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2023-11-14 17:54:59 +0000

    module export

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-class Rectangle {
+module.exports = class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
@@ -9,6 +9,4 @@ class Rectangle {
       Object.create(null);
     }
   }
-}
-
-module.exports.Rectangle;
+};
