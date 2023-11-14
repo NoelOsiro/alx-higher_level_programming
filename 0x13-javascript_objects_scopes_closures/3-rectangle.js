f0a9c5664d11aa7e06cd032a7aabe12b44b7b38c
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2023-11-14 17:58:56 +0000

    print rectanlge

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
+      // If w or h is 0 or not a positive integer, create an empty object
+      Object.create(null);
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+};
