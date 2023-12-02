3acd43d121f25fea4ab948f36aaf787575a0e6a2
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2023-12-02 07:08:15 +0000

    pycodestyles

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,8 +4,13 @@ import urllib.request
 
 
 if __name__ == "__main__":
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
-    with urllib.request.urlopen(urllib.request.Request('https://intranet.hbtn.io/status', headers=headers)) as response:
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+        }
+    with urllib.request.urlopen(
+            urllib.request.Request(
+                'https://intranet.hbtn.io/status',
+                headers=headers)) as response:
         html = response.read()
         print("Body response:")
         print("\t- type: {}\n\t- content: {}".format(type(html), html))
