14b465c87a1cfbf68c73f30cf2783961969f309b
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2024-01-10 20:55:43 +0000

    semistd

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,18 @@
 #!/usr/bin/node
 const request = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+
 request(url, function (error, response, body) {
-  console.log(JSON.parse(body).title);
+  if (error) {
+    console.error('Error:', error);
+    process.exit(1);
+  }
+
+  try {
+    const title = JSON.parse(body).title;
+    console.log(title);
+  } catch (parseError) {
+    console.error('Error parsing response:', parseError);
+    process.exit(1);
+  }
 });
